7ec2dcb64c4b5ca0b37acbcee0f1fc9318b4b64b
Allie Green <allie_green@proton.me>
2023-05-14 00:57:05 +0200

    Handle exceptions during network communication

--- main.py
+++ main.py
@@ -19,26 +19,32 @@ while True:
         "query": in_location,
     }
 
-    response = requests.get(service, params)
-    data = json.loads(response.text)
+    try:
+        response = requests.get(service, params)
+        response.raise_for_status()
+        data = json.loads(response.text)
 
-    if data.get("success") is False:
-        info = data["error"]["info"]
-        code = data["error"]["code"]
-        type_description = data["error"]["type"].replace("_", " ").capitalize()
-        print(f'\n{info}\nError code: {code}. {type_description}.\n')
-        break
+        if data.get("success") is False:
+            info = data["error"]["info"]
+            code = data["error"]["code"]
+            type_description = data["error"]["type"].replace("_", " ").capitalize()
+            print(f'\n{info}\nError code: {code}. {type_description}.\n')
+            break
 
-    name = data["location"]["name"] 
-    region = data["location"]["region"]
-    country = data["location"]["country"]
-    temperature = data["current"]["temperature"]
-    wind = data["current"]["wind_speed"]
-    description = data["current"]["weather_descriptions"][0]
+        name = data["location"]["name"] 
+        region = data["location"]["region"]
+        country = data["location"]["country"]
+        temperature = data["current"]["temperature"]
+        wind = data["current"]["wind_speed"]
+        description = data["current"]["weather_descriptions"][0]
 
-    if name == region:
-        weather = f'\nThe weather in {name}, {country} is {description.lower()}.\nThe temperature is {temperature} degrees Celsius, with wind speeds of {wind} km/h.'
-    else: 
-        weather = f'\nThe weather in {name}, {region}, {country} is {description.lower()}.\nThe temperature is {temperature} degrees Celsius, with wind speeds of {wind} km/h.'
-    
-    print(weather)
+        if name == region:
+            weather = f'\nThe weather in {name}, {country} is {description.lower()}.\nThe temperature is {temperature} degrees Celsius, with wind speeds of {wind} km/h.'
+        else: 
+            weather = f'\nThe weather in {name}, {region}, {country} is {description.lower()}.\nThe temperature is {temperature} degrees Celsius, with wind speeds of {wind} km/h.'
+        
+        print(weather)
+        
+    except (requests.exceptions.RequestException, ValueError) as error:
+        print(f'\nAn error occurred: {str(error)}\n')
+        continue
